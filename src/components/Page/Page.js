dc3287070612164a795215d0f7e7f97a89ab1346
Robin Louis van Komen <robin@vkmedia.dk>
2019-05-13 07:58:47 +0200

    add simple seo

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,21 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Helmet } from 'react-helmet'
 
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Content from '../Content/Content'
 
-export default ({ page }) => {
-    const renderContent = () => {
-        return page.content.map((content, i) => {
+class Page extends Component {
+    componentDidMount = () => {
+        if(this.props.page.landingpage){
+            document.body.classList.add('landing-page')
+        }
+    }
+
+    componentWillUnmount = () => {
+        if (this.props.page.landingpage) {
+            document.body.classList.remove('landing-page')
+        }
+    }
+
+    renderContent = () => {
+        return this.props.page.content.map((content, i) => {
             return <Content key={i} content={content} />
         })
     }
 
-    return (
-        <>
-            <Header />
-            <main>{ renderContent() }</main>
-            <Footer />
-        </>
-    )
+    renderPageTitle = () => {
+        const title = this.props.page.seoTitle ? this.props.page.seoTitle : this.props.page.title
+        return title + ' | Julie - personlig træner'
+    }
+
+    render(){
+        return (
+            <>
+                <Helmet>
+                    <title>{ this.renderPageTitle() }</title>
+                    <meta name="description" content={ this.props.page.seoDescription } />
+                </Helmet>
+
+                <Header />
+                <main>{this.renderContent()}</main>
+                <Footer />
+            </>
+        )
+    }
 }
+
+export default Page
