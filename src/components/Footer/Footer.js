ec4694fc9179e93522083666ac047b4a21ea42c8
Robin Louis van Komen <robin@vkmedia.dk>
2019-05-21 07:52:11 +0200

    add content to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,51 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 
 import './Footer.scss'
 
 const Footer = ({ footer }) => {
-    const renderFooterContent = () => ({ __html: footer.content })
+    const renderLogo = () => {
+        const logoPath = footer.logo ? 'http://localhost' + footer.logo.path : null
+        
+        if(logoPath){
+            return <NavLink id="footer-logo" style={{ backgroundImage: "url(" + logoPath + ")" }} exact to="/"></NavLink>
+        }
+    }
+
+    const renderContent = () => <div className="content" dangerouslySetInnerHTML={ { __html: footer.content} }></div>
+
+    const renderSome = () => {
+        const renderFacebook = () => {
+            if(footer.facebook){
+                return <a href={footer.facebook} rel="noopener noreferrer" target="_blank"><i className="fab fa-facebook-square"></i></a>
+            }
+
+            return null
+        }
+
+        const renderInstagram = () => {
+            if(footer.instagram){
+                return <a href={footer.instagram} rel="noopener noreferrer" target="_blank"><i className="fab fa-instagram"></i></a>
+            }
+
+            return null
+        }
+
+        return (
+            <div className="some">
+                {renderFacebook()}
+                {renderInstagram()}
+            </div>
+        )
+    }
 
     return (
-        <footer dangerouslySetInnerHTML={ renderFooterContent() }></footer>
+        <footer>
+            { renderLogo() }
+            { renderContent() }
+            { renderSome() }
+        </footer>
     )
 }
 
