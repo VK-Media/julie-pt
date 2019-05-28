7918bb026ee0e28209c3825c8070d5f8496d485e
Robin Louis van Komen <robin@vkmedia.dk>
2019-05-28 18:15:55 +0200

    change api route

--- src/actions/pagesAction.js
+++ src/actions/pagesAction.js
@@ -5,7 +5,7 @@ import { FETCH_PAGES } from './types'
 export const fetchPages = () => async dispatch => {
     let response = { data: { pages: {} } }
 
-    response = await collections.get('/pages')
+    response = await collections.get('/Pages')
 
     dispatch({ 'type': FETCH_PAGES, 'payload': response.data.entries })
 }
