ead2b7a9004cc85b31fc41b60834a3269588096e
Robin Louis van Komen <robin@vkmedia.dk>
2019-06-17 18:11:43 +0200

    add more functionality to buttons

--- src/components/Content/Button/Button.js
+++ src/components/Content/Button/Button.js
@@ -1,14 +1,38 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import './Button.scss'
 
-export default ({ content }) => {
+const Button = ({ content }) => {
     const text = content.settings.text
     const url = content.settings.url
+    const page = content.settings.link
+    const asset = content.settings.download
     const target = content.settings.target ? content.settings.target : '_top'
-    const className = content.settings.alignment ? 'button ' + content.settings.alignment.toLowerCase() : 'button'
+    let className = ['button']
 
-    if (text !== '' && url !== '') return <div className={className}><a href={url} target={target}>{text}</a></div>
+    if (content.settings.alignment){
+        className.push(content.settings.alignment.toLowerCase())
+    }
+
+    if(text !== ''){
+        if(url !== ''){
+            return <div className={className.join(' ')}><a href={url} target={target}>{text}</a></div>
+        } else if(page !== ''){
+            return <div className={className.join(' ')}><a href={page.title_slug} target={target}>{text}</a></div>
+        } else if(asset !== ''){
+            console.log(asset)
+            return <div className={className.join(' ')}><a href={} download target={target}>{text}</a></div>
+        }
+    }
 
     return null
 }
+
+const mapStateToProps = state => {
+    return {
+        pages: state.pages
+    }
+}
+
+export default connect(mapStateToProps, {})(Button)
