6db669297bab37aaba43258b0f161231ed0a5c1e
Robin Louis van Komen <robin@vkmedia.dk>
2019-07-25 16:24:37 +0200

    add content to RecipeDetail component

--- src/components/Content/RecipeDetails/RecipeDetails.js
+++ src/components/Content/RecipeDetails/RecipeDetails.js
@@ -7,8 +7,28 @@ export default ({ content }) => {
 
 	return (
 		<section className="recipe-details">
-			<div className="icons" />
-			<div className="details" />
+			<div className="icons">
+				<div>
+					<i class="fas fa-utensils fa-2x" />
+					<p>{content.portions}</p>
+				</div>
+				<div>
+					<i class="fas fa-clock" />
+					<p>{content.preparationTime}</p>
+				</div>
+			</div>
+			<div className="details">
+				<div
+					dangerouslySetInnerHTML={{
+						__html: content.ingredients
+					}}
+				/>
+				<div
+					dangerouslySetInnerHTML={{
+						__html: content.method
+					}}
+				/>
+			</div>
 		</section>
 	);
 };
