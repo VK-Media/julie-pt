d6da2d8611e93e916470ed31ad6d4bf2c327f8f9
Robin Louis van Komen <robin@vkmedia.dk>
2020-05-23 14:47:21 +0200

    add recipes search

--- src/components/Content/Recipes/Recipes.js
+++ src/components/Content/Recipes/Recipes.js
@@ -1,134 +1,197 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import _, { find } from "lodash";
-import "./Recipes.scss";
-import { NavLink } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import _, { find } from 'lodash'
+import './Recipes.scss'
+import { NavLink } from 'react-router-dom'
 
 const Recipes = ({ content }) => {
-	const recipes = useSelector(state => state.recipes);
-	const [searchWords, setSearchWords] = useState("");
-	const [selectedCategories, setSelectedCategories] = useState([]);
-	const selectedRecipes = content.settings.recipes.map(recipeData => {
-		return find(recipes, recipe => recipe._id === recipeData._id);
-	});
+    const recipes = useSelector(state => state.recipes)
+    const [searchWords, setSearchWords] = useState('')
+    const [selectedCategories, setSelectedCategories] = useState([])
+    const [selectedRecipes, setSelectedRecipes] = useState([])
 
-	const renderCategories = (categories) => {
-		if (categories) {
-			const categoryElements = categories.map(category => {
-				return <div key={category} className="category">{category}</div>;
-			});
+    useEffect(() => {
+        const initialRecipes = content.settings.recipes.map(recipeData => {
+            return find(recipes, recipe => recipe._id === recipeData._id)
+        })
 
-			if (categoryElements.length) {
-				return <div className="recipe__categories">{categoryElements}</div>;
-			}
-		}
+        if (initialRecipes.length) {
+            setSelectedRecipes(initialRecipes)
+        }
+    }, [content, recipes])
 
-		return null;
-	};
+    const searchRecipes = (searchWordsInput, categoriesInput) => {
+        const initialRecipes = content.settings.recipes.map(recipeData => {
+            return find(recipes, recipe => recipe._id === recipeData._id)
+        })
+        const searchWordsArray = searchWordsInput.split(' ')
+        const matchingRecipes = initialRecipes.filter(recipe => {
+            if (recipe) {
+                let containsSearchWords = true
+                let containsCategories = false
 
-	const getImageUrl = (recipe) => {
-		const firstImage = _.find(recipe.content, element => element.component === "image");
-		const image = firstImage ? firstImage.settings.image : null;
+                if (Array.isArray(categoriesInput) && categoriesInput.length) {
+                    if (Array.isArray(recipe.category)) {
+                        containsCategories = recipe.category.some(category => categoriesInput.includes(category))
+                    }
+                } else {
+                    containsCategories = true
+                }
 
-		if (image) return "https://admin.julie-pt.dk/" + image.path;
+                if (searchWordsInput.length) {
+                    for (const word of searchWordsArray) {
+                        const lowerCaseTitle = recipe.title.toLowerCase()
+                        const lowerCaseWord = word.toLowerCase()
 
-		return null;
-	};
+                        if (lowerCaseTitle.indexOf(lowerCaseWord) === -1) {
+                            containsSearchWords = false
+                        }
+                    }
+                } else {
+                    containsSearchWords = true
+                }
 
-	const renderImage = (recipe) => {
-		if (recipe) {
-			const imageUrl = getImageUrl(recipe);
+                return containsSearchWords && containsCategories
+            }
 
-			if (imageUrl) {
-				return <div className="recipe__image" style={{ backgroundImage: `url("${imageUrl}")` }}/>;
-			}
-		}
+            return false
+        })
 
-		return null;
-	};
+        setSelectedRecipes(matchingRecipes)
+    }
 
-	const renderSearchBar = () => {
-		return (
-			<div className="recipes__search-bar">
-				<input
-					type="text"
-					value={searchWords}
-					onChange={(event) => setSearchWords(event.target.value)}
-					placeholder="Indtast søgeord..."
-				/>
-			</div>
-		);
-	};
+    const renderCategories = (categories) => {
+        if (categories) {
+            const categoryElements = categories.map(category => {
+                return <div key={category} className="category">{category}</div>
+            })
 
-	const handleCategoryOptionClick = (option) => {
-		if (selectedCategories.includes(option)) {
-			const index = selectedCategories.indexOf(option);
+            if (categoryElements.length) {
+                return <div className="recipe__categories">{categoryElements}</div>
+            }
+        }
 
-			if (index > -1) {
-				const newSelected = [...selectedCategories];
+        return null
+    }
 
-				newSelected.splice(index, 1);
-				setSelectedCategories(newSelected);
-			}
-		} else {
-			setSelectedCategories([...selectedCategories, option]);
-		}
-	};
+    const getImageUrl = (recipe) => {
+        const firstImage = _.find(recipe.content, element => element.component === 'image')
+        const image = firstImage ? firstImage.settings.image : null
 
-	const renderCategoryOption = (label) => {
-		const classes = ["categories__option"];
+        if (image) return 'https://admin.julie-pt.dk/' + image.path
 
-		if (selectedCategories.includes(label)) {
-			classes.push("active");
-		}
+        return null
+    }
 
-		return <div key={label} className={classes.join(" ")} onClick={() => handleCategoryOptionClick(label)}>{label}</div>;
-	};
+    const renderImage = (recipe) => {
+        if (recipe) {
+            const imageUrl = getImageUrl(recipe)
 
-	const renderCategoryFilters = () => {
-		const options = ["Morgenmad", "Frokost", "Aftensmad", "Snack"];
-		const optionElements = options.map(label => {
-			return renderCategoryOption(label);
-		});
+            if (imageUrl) {
+                return <div className="recipe__image" style={{ backgroundImage: `url("${imageUrl}")` }}/>
+            }
+        }
 
-		return (
-			<div className="recipes__category-filters">
-				{optionElements}
-			</div>
-		);
-	};
+        return null
+    }
 
-	const renderRecipes = () => {
-		if (!content.settings.recipes) return null;
+    const handleSearchBarInput = (event) => {
+        const newValue = event.target.value
+        setSearchWords(newValue)
 
-		const recipesElements = selectedRecipes.map(recipe => {
-			if (recipe) {
-				return (
-					<NavLink key={recipe._id} className="recipe" to={`/opskrifter/${recipe.title_slug}`}>
-						{renderImage(recipe)}
-						<div className="recipe__title">{recipe.title}</div>
-						{renderCategories(recipe.category)}
-					</NavLink>
-				);
-			}
+        searchRecipes(newValue, selectedCategories)
+    }
 
-			return null;
-		});
+    const renderSearchBar = () => {
+        return (
+            <div className="recipes__search-bar">
+                <input
+                    type="text"
+                    value={searchWords}
+                    onChange={(event) => handleSearchBarInput(event)}
+                    placeholder="Indtast søgeord..."
+                />
+            </div>
+        )
+    }
 
-		if (recipesElements.length) {
-			return <div className="recipes__container">{recipesElements}</div>;
-		}
+    const handleCategoryOptionClick = (option) => {
+        let newCategories = []
+        if (selectedCategories.includes(option)) {
+            const index = selectedCategories.indexOf(option)
 
-		return null;
-	};
+            if (index > -1) {
+                newCategories = [...selectedCategories]
 
-	return (
-		<div className="recipes component">
-			{renderSearchBar()}
-			{renderCategoryFilters()}
-			{renderRecipes()}
-		</div>
-	);
-};
+                newCategories.splice(index, 1)
+                setSelectedCategories(newCategories)
+            }
+        } else {
+            newCategories = [...selectedCategories, option]
+            setSelectedCategories(newCategories)
+        }
 
-export default Recipes;
+        searchRecipes(searchWords, newCategories)
+    }
+
+    const renderCategoryOption = (label) => {
+        const classes = ['categories__option']
+
+        if (selectedCategories.includes(label)) {
+            classes.push('active')
+        }
+
+        return <div
+            key={label}
+            className={classes.join(' ')}
+            onClick={() => handleCategoryOptionClick(label)}
+        >{label}</div>
+    }
+
+    const renderCategoryFilters = () => {
+        const options = ['Morgenmad', 'Frokost', 'Aftensmad', 'Snack']
+        const optionElements = options.map(label => {
+            return renderCategoryOption(label)
+        })
+
+        return (
+            <div className="recipes__category-filters">
+                {optionElements}
+            </div>
+        )
+    }
+
+    const renderRecipes = () => {
+        if (!content.settings.recipes) return null
+
+        const recipesElements = selectedRecipes.map(recipe => {
+            if (recipe) {
+                return (
+                    <NavLink key={recipe._id} className="recipe" to={`/opskrifter/${recipe.title_slug}`}>
+                        {renderImage(recipe)}
+                        <div className="recipe__title">{recipe.title}</div>
+                        {renderCategories(recipe.category)}
+                    </NavLink>
+                )
+            }
+
+            return null
+        })
+
+        if (recipesElements.length) {
+            return <div className="recipes__container">{recipesElements}</div>
+        }
+
+        return <div>Der er på nuværende ingen opskrifter som matcher din søgning...</div>
+    }
+
+    return (
+        <div className="recipes component">
+            {renderSearchBar()}
+            {renderCategoryFilters()}
+            {renderRecipes()}
+        </div>
+    )
+}
+
+export default Recipes
